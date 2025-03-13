0257c48e784fa45a6ff37d1d890c9f760826b161
jam905 <jaminatkazbekova@yandex.ru>
2025-03-13 21:59:15 +0300

    Initial commit

---
+++ buttonclicker/src/main/java/com/mirea/kazbekovajm/buttonclicker/MainActivity.java
@@ -0,0 +1,51 @@
+package com.mirea.kazbekovajm.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });*/
+        setContentView(R.layout.activity_main);
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkBox);
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 9");
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+    public void onMyButtonClick(View view){
+        Toast.makeText(this, "Это не я!", Toast.LENGTH_SHORT).show();
+        checkBox.setChecked(!checkBox.isChecked());
+    }
+}
